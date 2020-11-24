b4842a4016f3bcf55a04962b325aab7358972da6
Даниил Гаренских <ghostik007@yandex.ru>
2020-11-24 15:26:33 +0300

    Burning Man V1.2

--- Burning_Man/js/site.js
+++ Burning_Man/js/site.js
@@ -6,4 +6,13 @@ $(function (){
 	$('.menu').click(function(){
 		$('.menu').toggleClass('show-menu')
 	})
+
+$("a.scroll-to").on("click", function(e){
+    e.preventDefault();
+    var anchor = $(this).attr('href');
+    $('html, body').stop().animate({
+        scrollTop: $(anchor).offset().top - 60
+    }, 800);
+});
+
 })
